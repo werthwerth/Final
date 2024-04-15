db1e1ad981dfc1c5abe7b1251ab3d8b47a79c1c4
werthwerth <teleperedozik@mail.ru>
2024-04-15 15:39:32 +0300

    цвааввыаыва

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using Final.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Final.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            Header _header = new Header();
+            return View(_header);
+        }
+        [HttpPost]
+        public IActionResult Register(string login, string password)
+        {
+            Header _header = new Header(login, password);
+            return View(_header);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Header.cs
@@ -0,0 +1,32 @@
+﻿namespace Final.Models
+{
+    public class Header
+    {
+        public Header()
+        {
+            registerButtonName = "Register";
+            registerButtonController = "Register";
+            registerButtonAction = "Register";
+            loginButtonName = "Login";
+            loginButtonController = "Home";
+            loginButtonAction = "Privacy";
+        }
+        public Header(string _login, string _password)
+        {
+            registerButtonName = $"{_login}/{_password}";
+            registerButtonController = "Register";
+            registerButtonAction = "Register";
+            loginButtonName = "Login";
+            loginButtonController = "Home";
+            loginButtonAction = "Privacy";
+        }
+        public string? registerButtonName { get; set; }
+        public string? registerButtonController { get; set; }
+        public string? registerButtonAction { get; set; }
+        public string? loginButtonName { get; set; }
+        public string? loginButtonController { get; set; }
+        public string? loginButtonAction { get; set; }
+        public bool? isLogged {  get; set; }
+        public string? login {  get; set; }
+    }
+}
